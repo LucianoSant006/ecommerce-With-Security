faaded42528fae605d3d0c416ec5ab9310bc9313
LucianoSant006 <santiagoluciano383@gmail.com>
2025-01-13 21:29:34 -0300

    Saving Order

--- src/main/java/com/devsuperior/dscommerce/services/OrderService.java
+++ src/main/java/com/devsuperior/dscommerce/services/OrderService.java
@@ -2,12 +2,19 @@ package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.OrderDTO;
 
-import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.dto.OrderItemDTO;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.*;
+import com.devsuperior.dscommerce.repositories.OrderItemRepository;
 import com.devsuperior.dscommerce.repositories.OrderRepository;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.ArrayList;
 
 @Service
 public class OrderService {
@@ -15,10 +22,40 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
     @Transactional(readOnly = true)
     public OrderDTO findById(Long id) {
         Order order = repository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Recurso não encontrado"));
         return new OrderDTO(order);
     }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order entity = new Order();
+        entity.setMoment(Instant.now());
+        entity.setStatus(OrderStatus.WAITING_PAYMENT);
+
+        User user = userService.authenticated();
+        entity.setClient(user);
+
+        for (OrderItemDTO itemDto : dto.getItems()) {
+            Product product = productRepository.getReferenceById(itemDto.getProductId());
+            OrderItem item = new OrderItem(entity, product, itemDto.getQuantity(), product.getPrice());
+            entity.getItems().add(item);
+        }
+        repository.save(entity);
+        orderItemRepository.saveAll(entity.getItems());
+        return new OrderDTO(entity);
+
+    }
+
 }
